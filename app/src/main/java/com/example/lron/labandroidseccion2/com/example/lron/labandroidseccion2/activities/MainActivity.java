9788d9e88baf5f6e7b3267c6af2213a134495606
Lucas Ron <lucas.ron@gire.com>
2017-10-02 15:30:57 -0300

    refactor paquetes

--- app/src/main/java/com/example/lron/labandroidseccion2/com/example/lron/labandroidseccion2/activities/MainActivity.java
+++ app/src/main/java/com/example/lron/labandroidseccion2/com/example/lron/labandroidseccion2/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.lron.labandroidseccion2;
+package com.example.lron.labandroidseccion2.com.example.lron.labandroidseccion2.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.lron.labandroidseccion2.R;
 
 public class MainActivity extends AppCompatActivity {
 
